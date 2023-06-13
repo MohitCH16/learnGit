15ed7312f806315391fb29c39d70a93de959c3da
Mohit Chandra Hansda <mohitju25@gmail.com>
2023-06-13 19:18:38 +0530

    adding all files

--- sum.java
+++ sum.java
@@ -2,8 +2,8 @@ class sum{
    public static void main(String args[]){
        int n = 3;
        int m = 4;
-       int s = n+m;
+       //int s = n+m;
 
-       System.out.println("Sum of "+n+" and "+m+" is "+s);
+       System.out.println("Sum of "+n+" and "+m+" is "+(n+m));
    }
 }	
