e0d993b88e57bb5ec415f1a00d1f003d5e9b8998
Mohit Chandra Hansda <mohitju25@gmail.com>
2023-06-13 19:08:35 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,9 @@
+class sum{
+   public static void main(String args[]){
+       int n = 3;
+       int m = 4;
+       int s = n+m;
+
+       System.out.println("Sum of "+n+" and "+m+" is "+s);
+   }
+}	
